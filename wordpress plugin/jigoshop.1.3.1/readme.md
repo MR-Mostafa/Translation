3315d7a6ca60f534115b1d91708fca274a95d063
IranWebDesign <mihanlicense@gmail.com>
2012-08-23 18:18:02 +0430

    upload jigoshop

---
+++ wordpress plugin/jigoshop.1.3.1/readme.md
@@ -0,0 +1,22 @@
+## Jigoshop v1.3.1
+
+Description :
+Jigoshop, a WordPress eCommerce plugin developed by professionals with years of experience delivering online shops for global brands. Complete your shop in minutes with physical and downloadable products or even services. Jigoshop provides you with the features necessary to set up an eCommerce web site lickety-split.
+With the option to create a multitude of product types and apply detailed attributes customers can easily refine your catalog, ensuring they find what they're looking for in just a couple of clicks.
+You can download the latest version from : please [click here](http://wordpress.org/extend/plugins/jigoshop/)
+
+### Translation :
+- Description : jigoshop v1.3.1 (just admin panel) is Compatibled with persian language. if you found any problem, Contact Us.
+- translation by : Mostafa Rahmati
+- Author website : http://iranwebdesign.ir
+
+For any **question** goto [issues](https://github.com/iranwebdesign/translation/issues), add lable and submit.
+
+Contributing
+------------
+1. Fork it.
+2. Create a branch (`git checkout -b my_markup`)
+3. Commit your changes (`git commit -am "Added Snarkdown"`)
+4. Push to the branch (`git push origin my_markup`)
+5. Open a [Pull Request][1]
+6. Enjoy a refreshing Diet Coke and wait
---
+++ wordpress plugin/jigoshop.1.3.1/shipping/shipping_method.class.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Shipping method class
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add directly to this file if you wish to upgrade Jigoshop to newer
+ * versions in the future. If you wish to customise Jigoshop core for your needs,
+ * please use our GitHub repository to publish essential changes for consideration.
+ *
+ * @package             Jigoshop
+ * @category            Checkout
+ * @author              Jigowatt
+ * @copyright           Copyright © 2011-2012 Jigowatt Ltd.
+ * @license             http://jigoshop.com/license/commercial-edition
+ */
+class jigoshop_shipping_method {
+
+	var $id;
+	var $title;
+	var $availability;
+	var $countries;
+	var $type;
+	var $cost				= 0;
+	var $fee				= 0;
+	var $min_amount			= null;
+	var $enabled			= false;
+	var $chosen				= false;
+	var $shipping_total 	= 0;
+	var $shipping_tax 		= 0;
+    
+    private $tax;
+    private $error_message = null;
+    
+    public function __construct() {        
+        Jigoshop_Base::get_options()->install_external_options_onto_tab( 'Shipping', $this->get_default_options() );
+    }
+
+    public function is_available() {
+
+    	if ($this->get_enabled()=="no") return false;
+
+		if (isset(jigoshop_cart::$cart_contents_total_ex_dl) && isset($this->min_amount) && $this->min_amount && $this->min_amount > jigoshop_cart::$cart_contents_total_ex_dl) return false;
+
+		if (is_array($this->get_ship_to_countries())) :
+			if (!in_array(jigoshop_customer::get_shipping_country(), $this->get_ship_to_countries())) :
+                $this->set_error_message('Sorry, it seems that there are no available shipping methods to your location. Please contact us if you require assistance or wish to make alternate arrangements.');
+                return false;
+            endif;
+		endif;
+
+		return true;
+
+    }
+
+    protected function get_ship_to_countries() {
+		$ship_to_countries = '';
+
+		if ($this->availability == 'specific') :
+			$ship_to_countries = $this->countries;
+		else :
+			if (Jigoshop_Base::get_options()->get_option('jigoshop_allowed_countries')=='specific') :
+				$ship_to_countries = Jigoshop_Base::get_options()->get_option('jigoshop_specific_allowed_countries');
+			endif;
+		endif;
+
+        return $ship_to_countries;
+    }
+
+    public function get_error_message() {
+    	return $this->error_message;
+    }
+
+    public function set_error_message($error_message = null) {
+    	$this->error_message = $error_message;
+    }
+
+    public function get_enabled() {
+        return $this->enabled;
+    }
+
+    /**
+     * sets the tax class to shipping_method. Needed to maintain current tax
+     * state from the shopping cart.
+     *
+     * @param type $tax jigoshop_tax instance
+     */
+    public function set_tax($tax) {
+        $this->tax = $tax;
+    }
+
+    protected function get_tax() {
+        return $this->tax;
+    }
+
+    public function get_fee( $fee, $total ) {
+		if (strstr($fee, '%')) :
+			return ($total/100) * str_replace('%', '', $fee);
+		else :
+			return $fee;
+		endif;
+	}
+
+	public function is_chosen() {
+    	if ($this->chosen) return true;
+    	return false;
+    }
+
+    public function choose() {
+    	$this->chosen = true;
+    	jigoshop_session::instance()->chosen_shipping_method_id = $this->id;
+    }
+
+    public function reset_method() {
+    	$this->chosen = false;
+    	$this->shipping_total = 0;
+    	$this->shipping_tax = 0;
+        $this->tax = null;
+    }
+
+    /**
+     * @deprecated - use get_default_options()
+     */
+    public function admin_options() {}
+
+    /**
+     * @deprecated - use get_default_options()
+     */
+    public function process_admin_options() {}
+
+    /**
+	 * Default Option settings for WordPress Settings API using an implementation of the Jigoshop_Options_Interface
+	 *
+	 * These should be installed on the Jigoshop_Options 'Shipping' tab
+     * @since 1.3
+	 *
+	 */	
+    protected function get_default_options() {
+        return array();
+    }
+
+}
